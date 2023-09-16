aaa6b1aed254f784a9a61eebec8f79473bd932d8
TG: @ItzAditya_xD <125627906+Akeno9@users.noreply.github.com>
2023-09-17 00:08:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+os.system("pip3 install -U -r requirements.txt && nohup python3 waifu.py &")
